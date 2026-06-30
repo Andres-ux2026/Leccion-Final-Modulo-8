db4fe762561d63b0b970bc8ec56086f9a34588ca
Andres-ux2026 <moraga_2006@hotmail.com>
2026-06-30 20:57:38 +0000

    Redirect / to /products/

--- conf/urls.py
+++ conf/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", lambda request: redirect("products/")),
     path("admin/", admin.site.urls),
     path("products/", include("productos.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
